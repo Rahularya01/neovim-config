8573ff65640d27c66b99ab8ce15551c034a4c34e
Rahul Arya <aryarahul819@gmail.com>
2025-05-04 13:06:39 +0530

    fix

--- lua/rahul/plugins/gitsigns.lua
+++ lua/rahul/plugins/gitsigns.lua
@@ -2,46 +2,102 @@ return {
 	"lewis6991/gitsigns.nvim",
 	event = { "BufReadPre", "BufNewFile" },
 	opts = {
+		-- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+		-- 1) Custom signs with number- and line-highlights
+		-- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+		signs = {
+			add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+			change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+			delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+			topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+			changedelete = {
+				hl = "GitSignsChange",
+				text = "~",
+				numhl = "GitSignsChangeNr",
+				linehl = "GitSignsChangeLn",
+			},
+		},
+
+		-- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+		-- 2) Enable extra diff features
+		-- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+		signcolumn = true, -- always show the sign column
+		numhl = false,
+		linehl = false,
+		word_diff = true, -- highlight word-level changes
+		watch_gitdir = { interval = 1000, follow_files = true },
+		attach_to_untracked = true,
+
+		-- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+		-- 3) Inline blame with delay and custom format
+		-- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+		current_line_blame = true, -- show blame info at end of line
+		current_line_blame_opts = {
+			virt_text = true,
+			virt_text_pos = "eol", -- end of line
+			delay = 200, -- ms
+			ignore_whitespace = false,
+		},
+		current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> • <summary>",
+
+		-- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+		-- 4) Floating preview window styling
+		-- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+		preview_config = {
+			border = "rounded",
+			style = "minimal",
+			relative = "cursor",
+			row = 0,
+			col = 1,
+		},
+
+		yadm = { enable = false },
+
+		-- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+		-- 5) Keymaps & on_attach
+		-- ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 		on_attach = function(bufnr)
 			local gs = package.loaded.gitsigns
-
-			local function map(mode, l, r, desc)
-				vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
+			local function map(mode, lhs, rhs, desc)
+				vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
 			end
 
-			-- Navigation
+			-- navigation
 			map("n", "]h", gs.next_hunk, "Next Hunk")
 			map("n", "[h", gs.prev_hunk, "Prev Hunk")
 
-			-- Actions
-			map("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
-			map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
+			-- staging/resetting
+			map("n", "<leader>hs", gs.stage_hunk, "Stage Hunk")
+			map("n", "<leader>hr", gs.reset_hunk, "Reset Hunk")
 			map("v", "<leader>hs", function()
 				gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-			end, "Stage hunk")
+			end, "Stage Hunk (visual)")
 			map("v", "<leader>hr", function()
 				gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-			end, "Reset hunk")
+			end, "Reset Hunk (visual)")
 
-			map("n", "<leader>hS", gs.stage_buffer, "Stage buffer")
-			map("n", "<leader>hR", gs.reset_buffer, "Reset buffer")
+			map("n", "<leader>hS", gs.stage_buffer, "Stage Buffer")
+			map("n", "<leader>hR", gs.reset_buffer, "Reset Buffer")
+			map("n", "<leader>hu", gs.undo_stage_hunk, "Undo Stage Hunk")
 
-			map("n", "<leader>hu", gs.undo_stage_hunk, "Undo stage hunk")
-
-			map("n", "<leader>hp", gs.preview_hunk, "Preview hunk")
-
-			map("n", "<leader>hb", function()
-				gs.blame_line({ full = true })
-			end, "Blame line")
-			map("n", "<leader>hB", gs.toggle_current_line_blame, "Toggle line blame")
-
-			map("n", "<leader>hd", gs.diffthis, "Diff this")
+			-- previews & diffs
+			map("n", "<leader>hp", gs.preview_hunk, "Preview Hunk")
+			map("n", "<leader>hd", gs.diffthis, "Diff This")
 			map("n", "<leader>hD", function()
 				gs.diffthis("~")
-			end, "Diff this ~")
+			end, "Diff This ~")
 
-			-- Text object
-			map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "Gitsigns select hunk")
+			-- blame
+			map("n", "<leader>hb", function()
+				gs.blame_line({ full = true })
+			end, "Full Blame")
+			map("n", "<leader>hB", gs.toggle_current_line_blame, "Toggle Blame")
+
+			-- word-diff toggle
+			map("n", "<leader>hw", gs.toggle_word_diff, "Toggle Word Diff")
+
+			-- text object
+			map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "Select Hunk")
 		end,
 	},
 }
