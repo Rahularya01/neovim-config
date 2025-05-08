d3f927b781a6ac0c422883822c7ae76597b5a026
Rahularya01 <aryarahul819@gmail.com>
2025-05-08 11:18:51 +0530

    new code

--- lua/rahul/plugins/nvim-cmp.lua
+++ lua/rahul/plugins/nvim-cmp.lua
@@ -1,176 +1,176 @@
 return {
-	"hrsh7th/nvim-cmp",
-	event = { "InsertEnter", "CmdlineEnter" }, -- Enable completion in command mode too
-	dependencies = {
+  "hrsh7th/nvim-cmp",
+  event = { "InsertEnter", "CmdlineEnter" }, -- Enable completion in command mode too
+  dependencies = {
 
-		"hrsh7th/cmp-buffer", -- source for text in buffer
-		"hrsh7th/cmp-path", -- source for file system paths
-		"hrsh7th/cmp-cmdline", -- source for vim command line
-		"hrsh7th/cmp-nvim-lsp", -- source for neovim's built-in LSP
-		"hrsh7th/cmp-nvim-lua", -- source for Neovim Lua API
-		{
-			"L3MON4D3/LuaSnip",
-			version = "v2.*",
-			build = "make install_jsregexp",
-			dependencies = {
-				"rafamadriz/friendly-snippets", -- useful snippets
-			},
-		},
-		"saadparwaiz1/cmp_luasnip", -- for autocompletion
-		"onsails/lspkind.nvim", -- vs-code like pictograms
-		"windwp/nvim-autopairs", -- auto close brackets, quotes, etc.
-	},
-	config = function()
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
-		local lspkind = require("lspkind")
-		local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    "hrsh7th/cmp-buffer",   -- source for text in buffer
+    "hrsh7th/cmp-path",     -- source for file system paths
+    "hrsh7th/cmp-cmdline",  -- source for vim command line
+    "hrsh7th/cmp-nvim-lsp", -- source for neovim's built-in LSP
+    "hrsh7th/cmp-nvim-lua", -- source for Neovim Lua API
+    {
+      "L3MON4D3/LuaSnip",
+      version = "v2.*",
+      build = "make install_jsregexp",
+      dependencies = {
+        "rafamadriz/friendly-snippets", -- useful snippets
+      },
+    },
+    "saadparwaiz1/cmp_luasnip", -- for autocompletion
+    "onsails/lspkind.nvim",     -- vs-code like pictograms
+    "windwp/nvim-autopairs",    -- auto close brackets, quotes, etc.
+  },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    local lspkind = require("lspkind")
+    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
-		-- Configure nvim-autopairs
-		require("nvim-autopairs").setup({
-			check_ts = true, -- Use treesitter to check for pairs
-			fast_wrap = {
-				map = "<M-e>", -- Alt+e to fast wrap
-				chars = { "{", "[", "(", '"', "'" },
-				pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
-				offset = 0, -- Offset from pattern match
-				end_key = "$",
-				keys = "qwertyuiopzxcvbnmasdfghjkl",
-				check_comma = true,
-				highlight = "PmenuSel",
-				highlight_grey = "LineNr",
-			},
-		})
+    -- Configure nvim-autopairs
+    require("nvim-autopairs").setup({
+      check_ts = true, -- Use treesitter to check for pairs
+      fast_wrap = {
+        map = "<M-e>", -- Alt+e to fast wrap
+        chars = { "{", "[", "(", '"', "'" },
+        pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+        offset = 0, -- Offset from pattern match
+        end_key = "$",
+        keys = "qwertyuiopzxcvbnmasdfghjkl",
+        check_comma = true,
+        highlight = "PmenuSel",
+        highlight_grey = "LineNr",
+      },
+    })
 
-		-- Hook autopairs into cmp
-		cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    -- Hook autopairs into cmp
+    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
-		-- Load VSCode snippets
-		require("luasnip.loaders.from_vscode").lazy_load()
+    -- Load VSCode snippets
+    require("luasnip.loaders.from_vscode").lazy_load()
 
-		-- Add some useful snippets programmatically
-		luasnip.add_snippets("all", {
-			luasnip.snippet("date", {
-				luasnip.function_node(function()
-					return os.date("%Y-%m-%d")
-				end, {}),
-			}),
-		})
+    -- Add some useful snippets programmatically
+    luasnip.add_snippets("all", {
+      luasnip.snippet("date", {
+        luasnip.function_node(function()
+          return os.date("%Y-%m-%d")
+        end, {}),
+      }),
+    })
 
-		-- Helper function for super-tab functionality
-		local has_words_before = function()
-			unpack = unpack or table.unpack
-			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-		end
+    -- Helper function for super-tab functionality
+    local has_words_before = function()
+      unpack = unpack or table.unpack
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    end
 
-		-- Setup completion
-		cmp.setup({
-			completion = {
-				completeopt = "menu,menuone,preview,noselect",
-			},
-			window = {
-				completion = cmp.config.window.bordered({
-					winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
-					border = "rounded",
-					scrollbar = true,
-				}),
-				documentation = cmp.config.window.bordered({
-					winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
-					border = "rounded",
-					scrollbar = true,
-				}),
-			},
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-k>"] = cmp.mapping.select_prev_item(),
-				["<C-j>"] = cmp.mapping.select_next_item(),
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete(),
-				["<C-e>"] = cmp.mapping.abort(),
-				["<CR>"] = cmp.mapping.confirm({ select = false }),
-			}),
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp", priority = 1000 },
-				{ name = "nvim_lua", priority = 900 },
-				{ name = "luasnip", priority = 800 },
-				{ name = "buffer", priority = 700 },
-				{ name = "path", priority = 600 },
-			}),
-			formatting = {
-				format = lspkind.cmp_format({
-					mode = "symbol_text", -- Show both symbol and text
-					maxwidth = 50,
-					ellipsis_char = "...",
-					menu = {
-						buffer = "[Buffer]",
-						nvim_lsp = "[LSP]",
-						nvim_lua = "[API]",
-						path = "[Path]",
-						luasnip = "[Snippet]",
-					},
-					before = function(entry, vim_item)
-						-- Get the full snippet text
-						if entry.source.name == "luasnip" then
-							vim_item.abbr = string.format("%s %s", vim_item.abbr, "[Snippet]")
-						end
-						return vim_item
-					end,
-				}),
-			},
-			sorting = {
-				comparators = {
-					cmp.config.compare.offset,
-					cmp.config.compare.exact,
-					cmp.config.compare.score,
-					-- Prioritize snippets
-					function(entry1, entry2)
-						local types = require("cmp.types")
-						if entry1.source.name == "luasnip" and entry2.source.name ~= "luasnip" then
-							return true
-						elseif entry1.source.name ~= "luasnip" and entry2.source.name == "luasnip" then
-							return false
-						end
-						return nil
-					end,
-					cmp.config.compare.kind,
-					cmp.config.compare.sort_text,
-					cmp.config.compare.length,
-					cmp.config.compare.order,
-				},
-			},
-			experimental = {
-				ghost_text = true, -- Show future text as ghost text
-			},
-		})
+    -- Setup completion
+    cmp.setup({
+      completion = {
+        completeopt = "menu,menuone,preview,noselect",
+      },
+      window = {
+        completion = cmp.config.window.bordered({
+          winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
+          border = "rounded",
+          scrollbar = true,
+        }),
+        documentation = cmp.config.window.bordered({
+          winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
+          border = "rounded",
+          scrollbar = true,
+        }),
+      },
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<C-k>"] = cmp.mapping.select_prev_item(),
+        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp", priority = 1000 },
+        { name = "nvim_lua", priority = 900 },
+        { name = "luasnip",  priority = 800 },
+        { name = "buffer",   priority = 700 },
+        { name = "path",     priority = 600 },
+      }),
+      formatting = {
+        format = lspkind.cmp_format({
+          mode = "symbol_text", -- Show both symbol and text
+          maxwidth = 50,
+          ellipsis_char = "...",
+          menu = {
+            buffer = "[Buffer]",
+            nvim_lsp = "[LSP]",
+            nvim_lua = "[API]",
+            path = "[Path]",
+            luasnip = "[Snippet]",
+          },
+          before = function(entry, vim_item)
+            -- Get the full snippet text
+            if entry.source.name == "luasnip" then
+              vim_item.abbr = string.format("%s %s", vim_item.abbr, "[Snippet]")
+            end
+            return vim_item
+          end,
+        }),
+      },
+      sorting = {
+        comparators = {
+          cmp.config.compare.offset,
+          cmp.config.compare.exact,
+          cmp.config.compare.score,
+          -- Prioritize snippets
+          function(entry1, entry2)
+            local types = require("cmp.types")
+            if entry1.source.name == "luasnip" and entry2.source.name ~= "luasnip" then
+              return true
+            elseif entry1.source.name ~= "luasnip" and entry2.source.name == "luasnip" then
+              return false
+            end
+            return nil
+          end,
+          cmp.config.compare.kind,
+          cmp.config.compare.sort_text,
+          cmp.config.compare.length,
+          cmp.config.compare.order,
+        },
+      },
+      experimental = {
+        ghost_text = true, -- Show future text as ghost text
+      },
+    })
 
-		-- Set up special configuration for specific filetypes
-		cmp.setup.filetype("gitcommit", {
-			sources = cmp.config.sources({
-				{ name = "buffer" },
-			}),
-		})
+    -- Set up special configuration for specific filetypes
+    cmp.setup.filetype("gitcommit", {
+      sources = cmp.config.sources({
+        { name = "buffer" },
+      }),
+    })
 
-		-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore)
-		cmp.setup.cmdline({ "/", "?" }, {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = {
-				{ name = "buffer" },
-			},
-		})
+    -- Use buffer source for / and ? (if you enabled native_menu, this won't work anymore)
+    cmp.setup.cmdline({ "/", "?" }, {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = {
+        { name = "buffer" },
+      },
+    })
 
-		-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore)
-		cmp.setup.cmdline(":", {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = cmp.config.sources({
-				{ name = "path" },
-				{ name = "cmdline" },
-			}),
-		})
-	end,
+    -- Use cmdline & path source for ':' (if you enabled native_menu, this won't work anymore)
+    cmp.setup.cmdline(":", {
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = "path" },
+        { name = "cmdline" },
+      }),
+    })
+  end,
 }
--- lua/rahul/plugins/spectre.lua
+++ lua/rahul/plugins/spectre.lua
@@ -1,197 +1,197 @@
 -- In your Lazy configuration (typically in lua/plugins.lua or similar)
 return {
-	{
-		"nvim-pack/nvim-spectre",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		-- Lazy-load on keymaps
-		keys = {
-			{ "<leader>S", mode = { "n" }, desc = "Toggle Spectre" },
-			{ "<leader>sw", mode = { "n" }, desc = "Search current word" },
-			{ "<leader>sv", mode = { "v" }, desc = "Search current selection" },
-			{ "<leader>sf", mode = { "n" }, desc = "Search in current file" },
-			{ "<leader>sp", mode = { "n" }, desc = "Search word in current file" },
-			{ "<leader>sr", mode = { "n" }, desc = "Resume last search" },
-		},
-		config = function()
-			local spectre = require("spectre")
-			spectre.setup({
-				color_devicons = true,
-				open_cmd = "vnew", -- Open spectre in a vertical split
-				live_update = true, -- Auto update as you type
-				line_sep_start = "┌──────────────────────────────────────────",
-				result_padding = "│  ",
-				line_sep = "└──────────────────────────────────────────",
-				highlight = {
-					ui = "String",
-					search = "DiffChange",
-					replace = "DiffDelete",
-				},
-				mapping = {
-					["toggle_line"] = {
-						map = "dd",
-						cmd = "<cmd>lua require('spectre').toggle_line()<CR>",
-						desc = "toggle current item",
-					},
-					["enter_file"] = {
-						map = "<cr>",
-						cmd = "<cmd>lua require('spectre.actions').select_entry()<CR>",
-						desc = "goto current file",
-					},
-					["send_to_qf"] = {
-						map = "<leader>q",
-						cmd = "<cmd>lua require('spectre.actions').send_to_qf()<CR>",
-						desc = "send all items to quickfix",
-					},
-					["replace_cmd"] = {
-						map = "<leader>c",
-						cmd = "<cmd>lua require('spectre.actions').replace_cmd()<CR>",
-						desc = "input replace command",
-					},
-					["show_option_menu"] = {
-						map = "<leader>o",
-						cmd = "<cmd>lua require('spectre').show_options()<CR>",
-						desc = "show options",
-					},
-					["run_replace"] = {
-						map = "<leader>r",
-						cmd = "<cmd>lua require('spectre.actions').run_replace()<CR>",
-						desc = "replace all",
-					},
-					["change_view_mode"] = {
-						map = "<leader>v",
-						cmd = "<cmd>lua require('spectre').change_view()<CR>",
-						desc = "change result view mode",
-					},
-					["toggle_ignore_case"] = {
-						map = "ti",
-						cmd = "<cmd>lua require('spectre').change_options('ignore-case')<CR>",
-						desc = "toggle ignore case",
-					},
-					["toggle_ignore_hidden"] = {
-						map = "th",
-						cmd = "<cmd>lua require('spectre').change_options('hidden')<CR>",
-						desc = "toggle search hidden",
-					},
-					["resume_last_search"] = {
-						map = "<leader>l",
-						cmd = "<cmd>lua require('spectre').resume_last_search()<CR>",
-						desc = "resume last search",
-					},
-				},
-				find_engine = {
-					-- rg is map with finder_cmd
-					["rg"] = {
-						cmd = "rg",
-						-- default args
-						args = {
-							"--color=never",
-							"--no-heading",
-							"--with-filename",
-							"--line-number",
-							"--column",
-						},
-						options = {
-							["ignore-case"] = {
-								value = "--ignore-case",
-								icon = "[I]",
-								desc = "ignore case",
-							},
-							["hidden"] = {
-								value = "--hidden",
-								desc = "hidden file",
-								icon = "[H]",
-							},
-							-- you can put any option you want here
-							["fixed-strings"] = {
-								value = "--fixed-strings",
-								desc = "fixed strings",
-								icon = "[F]",
-							},
-						},
-					},
-					["ag"] = {
-						cmd = "ag",
-						args = {
-							"--vimgrep",
-							"-s",
-						},
-						options = {
-							["ignore-case"] = {
-								value = "-i",
-								icon = "[I]",
-								desc = "ignore case",
-							},
-							["hidden"] = {
-								value = "--hidden",
-								desc = "hidden file",
-								icon = "[H]",
-							},
-						},
-					},
-				},
-				replace_engine = {
-					["sed"] = {
-						cmd = "sed",
-						args = nil,
-						options = {
-							["ignore-case"] = {
-								value = "--ignore-case",
-								icon = "[I]",
-								desc = "ignore case",
-							},
-						},
-					},
-					-- call rust code by nvim-oxi
-					["oxi"] = {
-						cmd = "oxi",
-						args = {},
-						options = {
-							["ignore-case"] = {
-								value = "i",
-								icon = "[I]",
-								desc = "ignore case",
-							},
-						},
-					},
-				},
-				default = {
-					find = {
-						--pick one of item in find_engine
-						cmd = "rg",
-						options = { "ignore-case" },
-					},
-					replace = {
-						--pick one of item in replace_engine
-						cmd = "sed",
-					},
-				},
-				replace_vim_cmd = "cfdo %s///g",
-				is_open_target_win = true, --open file on opener window
-				is_insert_mode = false, -- start open panel in insert mode
-				is_block_ui_break = true, -- prevent ui breaking when search too frequently
-			})
+  {
+    "nvim-pack/nvim-spectre",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- Lazy-load on keymaps
+    keys = {
+      { "<leader>S",  mode = { "n" }, desc = "Toggle Spectre" },
+      { "<leader>sw", mode = { "n" }, desc = "Search current word" },
+      { "<leader>sv", mode = { "v" }, desc = "Search current selection" },
+      { "<leader>sf", mode = { "n" }, desc = "Search in current file" },
+      { "<leader>sp", mode = { "n" }, desc = "Search word in current file" },
+      { "<leader>sr", mode = { "n" }, desc = "Resume last search" },
+    },
+    config = function()
+      local spectre = require("spectre")
+      spectre.setup({
+        color_devicons = true,
+        open_cmd = "vnew", -- Open spectre in a vertical split
+        live_update = true, -- Auto update as you type
+        line_sep_start = "┌──────────────────────────────────────────",
+        result_padding = "│  ",
+        line_sep = "└──────────────────────────────────────────",
+        highlight = {
+          ui = "String",
+          search = "DiffChange",
+          replace = "DiffDelete",
+        },
+        mapping = {
+          ["toggle_line"] = {
+            map = "dd",
+            cmd = "<cmd>lua require('spectre').toggle_line()<CR>",
+            desc = "toggle current item",
+          },
+          ["enter_file"] = {
+            map = "<cr>",
+            cmd = "<cmd>lua require('spectre.actions').select_entry()<CR>",
+            desc = "goto current file",
+          },
+          ["send_to_qf"] = {
+            map = "<leader>q",
+            cmd = "<cmd>lua require('spectre.actions').send_to_qf()<CR>",
+            desc = "send all items to quickfix",
+          },
+          ["replace_cmd"] = {
+            map = "<leader>c",
+            cmd = "<cmd>lua require('spectre.actions').replace_cmd()<CR>",
+            desc = "input replace command",
+          },
+          ["show_option_menu"] = {
+            map = "<leader>o",
+            cmd = "<cmd>lua require('spectre').show_options()<CR>",
+            desc = "show options",
+          },
+          ["run_replace"] = {
+            map = "<leader>r",
+            cmd = "<cmd>lua require('spectre.actions').run_replace()<CR>",
+            desc = "replace all",
+          },
+          ["change_view_mode"] = {
+            map = "<leader>v",
+            cmd = "<cmd>lua require('spectre').change_view()<CR>",
+            desc = "change result view mode",
+          },
+          ["toggle_ignore_case"] = {
+            map = "ti",
+            cmd = "<cmd>lua require('spectre').change_options('ignore-case')<CR>",
+            desc = "toggle ignore case",
+          },
+          ["toggle_ignore_hidden"] = {
+            map = "th",
+            cmd = "<cmd>lua require('spectre').change_options('hidden')<CR>",
+            desc = "toggle search hidden",
+          },
+          ["resume_last_search"] = {
+            map = "<leader>l",
+            cmd = "<cmd>lua require('spectre').resume_last_search()<CR>",
+            desc = "resume last search",
+          },
+        },
+        find_engine = {
+          -- rg is map with finder_cmd
+          ["rg"] = {
+            cmd = "rg",
+            -- default args
+            args = {
+              "--color=never",
+              "--no-heading",
+              "--with-filename",
+              "--line-number",
+              "--column",
+            },
+            options = {
+              ["ignore-case"] = {
+                value = "--ignore-case",
+                icon = "[I]",
+                desc = "ignore case",
+              },
+              ["hidden"] = {
+                value = "--hidden",
+                desc = "hidden file",
+                icon = "[H]",
+              },
+              -- you can put any option you want here
+              ["fixed-strings"] = {
+                value = "--fixed-strings",
+                desc = "fixed strings",
+                icon = "[F]",
+              },
+            },
+          },
+          ["ag"] = {
+            cmd = "ag",
+            args = {
+              "--vimgrep",
+              "-s",
+            },
+            options = {
+              ["ignore-case"] = {
+                value = "-i",
+                icon = "[I]",
+                desc = "ignore case",
+              },
+              ["hidden"] = {
+                value = "--hidden",
+                desc = "hidden file",
+                icon = "[H]",
+              },
+            },
+          },
+        },
+        replace_engine = {
+          ["sed"] = {
+            cmd = "sed",
+            args = nil,
+            options = {
+              ["ignore-case"] = {
+                value = "--ignore-case",
+                icon = "[I]",
+                desc = "ignore case",
+              },
+            },
+          },
+          -- call rust code by nvim-oxi
+          ["oxi"] = {
+            cmd = "oxi",
+            args = {},
+            options = {
+              ["ignore-case"] = {
+                value = "i",
+                icon = "[I]",
+                desc = "ignore case",
+              },
+            },
+          },
+        },
+        default = {
+          find = {
+            --pick one of item in find_engine
+            cmd = "rg",
+            options = { "ignore-case" },
+          },
+          replace = {
+            --pick one of item in replace_engine
+            cmd = "sed",
+          },
+        },
+        replace_vim_cmd = "cfdo %s///g",
+        is_open_target_win = true, --open file on opener window
+        is_insert_mode = false, -- start open panel in insert mode
+        is_block_ui_break = true, -- prevent ui breaking when search too frequently
+      })
 
-			-- Key mappings
-			vim.keymap.set("n", "<leader>S", '<cmd>lua require("spectre").toggle()<CR>', {
-				desc = "Toggle Spectre",
-			})
-			vim.keymap.set("n", "<leader>sw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
-				desc = "Search current word",
-			})
-			vim.keymap.set("v", "<leader>sv", '<esc><cmd>lua require("spectre").open_visual()<CR>', {
-				desc = "Search current selection",
-			})
-			vim.keymap.set("n", "<leader>sf", '<cmd>lua require("spectre").open_file_search()<CR>', {
-				desc = "Search in current file",
-			})
-			vim.keymap.set("n", "<leader>sp", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
-				desc = "Search word in current file",
-			})
-			vim.keymap.set("n", "<leader>sr", '<cmd>lua require("spectre").resume_last_search()<CR>', {
-				desc = "Resume last search",
-			})
-		end,
-	},
+      -- Key mappings
+      vim.keymap.set("n", "<leader>S", '<cmd>lua require("spectre").toggle()<CR>', {
+        desc = "Toggle Spectre",
+      })
+      vim.keymap.set("n", "<leader>sw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
+        desc = "Search current word",
+      })
+      vim.keymap.set("v", "<leader>sv", '<esc><cmd>lua require("spectre").open_visual()<CR>', {
+        desc = "Search current selection",
+      })
+      vim.keymap.set("n", "<leader>sf", '<cmd>lua require("spectre").open_file_search()<CR>', {
+        desc = "Search in current file",
+      })
+      vim.keymap.set("n", "<leader>sp", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', {
+        desc = "Search word in current file",
+      })
+      vim.keymap.set("n", "<leader>sr", '<cmd>lua require("spectre").resume_last_search()<CR>', {
+        desc = "Resume last search",
+      })
+    end,
+  },
 }
