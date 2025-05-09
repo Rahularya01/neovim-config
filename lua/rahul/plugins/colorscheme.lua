c462f61630b720dcd426fdbe581c724e449700b0
Rahul Arya <aryarahul819@gmail.com>
2025-05-10 00:03:29 +0530

    fiex

--- lua/rahul/plugins/colorscheme.lua
+++ lua/rahul/plugins/colorscheme.lua
@@ -1,15 +1,74 @@
 return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			require("catppuccin").setup({
-				flavour = "mocha", -- Set the theme flavor
-				transparent_background = true, -- Enable transparency if needed
-			})
-			-- Load the colorscheme
-			vim.cmd([[colorscheme catppuccin]])
-		end,
-	},
+  -- Catppuccin (your existing setup)
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        transparent_background = true,
+      })
+      -- vim.cmd([[colorscheme catppuccin]])
+    end,
+  },
+  -- Tokyo Night
+  {
+    "folke/tokyonight.nvim",
+    name = "tokyonight",
+    priority = 1000,
+    config = function()
+      require("tokyonight").setup({
+        style = "night",    -- night | storm | day
+        transparent = true, -- disables setting background color
+        terminal_colors = true,
+      })
+      vim.cmd([[colorscheme tokyonight]]) -- enable to use
+    end,
+  },
+
+  -- Kanagawa
+  {
+    "rebelot/kanagawa.nvim",
+    name = "kanagawa",
+    priority = 850,
+    config = function()
+      require("kanagawa").setup({
+        compile = false,
+        background = { -- map backgrounds to transparent
+          dark = "transparent",
+          light = "transparent",
+        },
+        transparent = true,
+        dimInactive = true,
+      })
+      -- vim.cmd([[colorscheme kanagawa]])  -- enable to use
+    end,
+  },
+
+  -- Rose Pine
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    priority = 800,
+    config = function()
+      require("rose-pine").setup({
+        dark_variant = "moon", -- pine | moon
+        disable_background = true,
+      })
+      -- vim.cmd([[colorscheme rose-pine]]) -- enable to use
+    end,
+  },
+
+  -- Gruvbox Material
+  {
+    "sainnhe/gruvbox-material",
+    name = "gruvbox-material",
+    priority = 750,
+    config = function()
+      vim.g.gruvbox_material_background = "hard" -- hard | medium | soft
+      vim.g.gruvbox_material_transparent_background = 1
+      -- vim.cmd([[colorscheme gruvbox-material]])  -- enable to use
+    end,
+  },
 }
