cdcdd90e988f6ae09604831d99f85c44b38d19af
Rahul Arya <aryarahul819@gmail.com>
2025-12-16 00:18:57 +0530

    initialize linting

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -7,6 +7,10 @@ return {
 		lint.linters_by_ft = {
 			python = { "pylint" },
 			lua = { "luacheck" },
+			javascript = { "eslint_d" },
+			typescript = { "eslint_d" },
+			javascriptreact = { "eslint_d" },
+			typescriptreact = { "eslint_d" },
 		}
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
