c01fe4f670a0949d434ac36ced94e398ec162326
Rahul Arya <aryarahul819@gmail.com>
2025-12-15 17:19:22 +0530

    fixes

---
+++ lua/plugins/ufo.lua
@@ -0,0 +1,21 @@
+return {
+	"kevinhwang91/nvim-ufo",
+	dependencies = { "kevinhwang91/promise-async" },
+	event = "BufReadPost",
+	config = function()
+		vim.opt.foldcolumn = "1"
+		vim.opt.foldlevel = 99
+		vim.opt.foldlevelstart = 99
+		vim.opt.foldenable = true
+		vim.opt.foldmethod = "manual"
+
+		require("ufo").setup({
+			provider_selector = function(bufnr, filetype, buftype)
+				return { "lsp", "indent" }
+			end,
+		})
+
+		vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+		vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+	end,
+}
