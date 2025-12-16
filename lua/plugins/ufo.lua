6a61c77082c67d340463134c52aeaef774ca2eae
Rahul Arya <aryarahul819@gmail.com>
2025-12-16 11:23:08 +0530

    feat: cuid-868gmqu3h canada sms dlc form

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -1,13 +1,12 @@
 return {
 	"kevinhwang91/nvim-ufo",
 	dependencies = { "kevinhwang91/promise-async" },
-	event = "BufReadPost",
+	event = "BufReadPre", -- or "BufReadPre" is arguably better for folding
 	config = function()
-		vim.opt.foldcolumn = "1"
+		vim.opt.foldcolumn = "0"
 		vim.opt.foldlevel = 99
 		vim.opt.foldlevelstart = 99
 		vim.opt.foldenable = true
-		vim.opt.foldmethod = "manual"
 
 		require("ufo").setup({
 			provider_selector = function(bufnr, filetype, buftype)
