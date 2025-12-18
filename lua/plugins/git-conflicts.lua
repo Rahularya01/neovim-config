e16708a0779d8d1eeac691c86d69b370b4aa42aa
Rahul Arya <aryarahul819@gmail.com>
2025-12-18 11:19:44 +0530

    fixes

--- lua/plugins/git-conflicts.lua
+++ lua/plugins/git-conflicts.lua
@@ -1,1 +1,19 @@
-return { "akinsho/git-conflict.nvim", version = "*", config = true }
+return {
+	"akinsho/git-conflict.nvim",
+	version = "*",
+	event = "VeryLazy",
+	config = function()
+		require("git-conflict").setup({
+			default_mappings = true,
+			list_opener = "copen",
+			highlights = {
+				incoming = "DiffAdd",
+				current = "DiffChange",
+				ancestor = "DiffDelete",
+			},
+		})
+
+		vim.api.nvim_set_hl(0, "GitConflictCurrent", { link = "DiffChange" })
+		vim.api.nvim_set_hl(0, "GitConflictIncoming", { link = "DiffAdd" })
+	end,
+}
