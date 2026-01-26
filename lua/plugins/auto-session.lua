fd9fb8bc9faa1d9d8d044b10118e5805a403d913
Rahul Arya <aryarahul819@gmail.com>
2026-01-26 13:55:39 +0530

    style(plugins): remove redundant comments in auto-session

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,12 +1,11 @@
 return {
 	"rmagatti/auto-session",
-	event = "VimEnter", -- Load after startup to not block, but early enough for session restore
+	event = "VimEnter",
 
 	config = function()
 		require("auto-session").setup({
 			log_level = "error",
 
-			-- Suppress session for these directories
 			suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
 
 			bypass_session_save_file_types = {
@@ -18,14 +17,12 @@ return {
 				"dapui_watches",
 			},
 
-			-- Fix: Run :edit on the current buffer after restore to trigger Treesitter/LSP
 			post_restore_cmds = {
 				function()
 					vim.cmd("edit")
 				end,
 			},
 
-			-- Keymaps
 			vim.keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session" }),
 			vim.keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session" }),
 		})
