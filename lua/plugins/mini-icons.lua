cce4de15dc7e3da67dfdf1d4ccd3a13feaa85506
Rahul Arya <aryarahul819@gmail.com>
2026-02-03 14:27:39 +0530

    fixes

---
+++ lua/plugins/mini-icons.lua
@@ -0,0 +1,13 @@
+return {
+  "echasnovski/mini.icons",
+  opts = {
+    file = {
+      ["dockerfile"] = { icon = "󰡨", str = "Dockerfile" },
+      ["makefile"] = { icon = "󰆧", str = "Makefile" },
+      ["justfile"] = { icon = "󰆧", str = "Justfile" },
+    },
+    filetype = {
+      dotenv = { icon = "", str = "Env" },
+    },
+  },
+}
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,15 +7,67 @@ return {
 		input = { enabled = true },
 		picker = {
 			enabled = true,
+			sources = {
+				gh_issue = {},
+				gh_pr = {},
+			},
 		},
 		dashboard = { enabled = true },
 		terminal = { enabled = true },
 		notifier = { enabled = true },
 		indent = { enabled = true },
 		lazygit = { enabled = true },
-		image = { enabled = true },
+		gh = {
+			enabled = true,
+		},
+		image = {
+			enabled = true,
+			doc = {
+				enabled = false,
+				max_width = 30,
+				max_height = 15,
+			},
+		},
 	},
+
 	keys = {
+		{
+			"<leader>gi",
+			function()
+				Snacks.picker.gh_issue()
+			end,
+			desc = "GitHub Issues (open)",
+		},
+		{
+			"<leader>gI",
+			function()
+				Snacks.picker.gh_issue({ state = "all" })
+			end,
+			desc = "GitHub Issues (all)",
+		},
+		{
+			"<leader>gp",
+			function()
+				Snacks.picker.gh_pr()
+			end,
+			desc = "GitHub Pull Requests (open)",
+		},
+		{
+			"<leader>gP",
+			function()
+				Snacks.picker.gh_pr({ state = "all" })
+			end,
+			desc = "GitHub Pull Requests (all)",
+		},
+		-- Image hover
+		{
+			"<S-j>",
+			function()
+				Snacks.image.hover()
+			end,
+			mode = { "n", "v" },
+			desc = "Image Hover",
+		},
 		-- Top Pickers
 		{
 			"<leader>.",
