a7b26f854e8fe2e365b2e95d1e940e325e5a8a91
Rahul Arya <aryarahul819@gmail.com>
2026-02-22 22:04:55 +0530

    fixes

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,7 +1,6 @@
 return {
 	"github/copilot.vim",
-	-- laod immidiately
-	event = "InsertEnter",
+	lazy = true,
 	cmd = "Copilot",
 	config = function()
 		vim.api.nvim_set_keymap("i", "<Tab>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
