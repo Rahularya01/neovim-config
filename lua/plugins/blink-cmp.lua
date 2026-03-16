41c1a6fd03fc2328bc989bee54249a636e2648ab
Rahul Arya <aryarahul819@gmail.com>
2026-03-16 14:06:46 +0530

    fixes

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -17,11 +17,13 @@ return {
 		},
 	},
 	opts = {
+
 		keymap = {
 			preset = "super-tab",
 			["<C-k>"] = { "select_prev", "fallback" },
 			["<C-j>"] = { "select_next", "fallback" },
 			["<CR>"] = { "accept", "fallback" },
+			["<C-space>"] = { "show", "fallback" },
 			["<C-e>"] = { "cancel", "fallback" },
 		},
 		appearance = {
