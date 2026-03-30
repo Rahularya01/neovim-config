232e0ce9e80099228a85aec48e7301375d37dcfe
Rahul Arya <aryarahul819@gmail.com>
2026-03-30 19:42:41 +0530

    chore: Replace Copilot with Supermaven

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,4 +1,14 @@
 return {
-	"github/copilot.vim",
+	"supermaven-inc/supermaven-nvim",
 	event = "BufReadPost",
+	opts = {
+		keymap = {
+			accept = "<Tab>",
+			clear = "<C-h>",
+			accept_word = "<C-l>",
+			accept_line = "<C-j>",
+			next = "<C-k>",
+			prev = "<C-;>",
+		},
+	},
 }
