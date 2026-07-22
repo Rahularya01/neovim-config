8398d57cbd74b327ca3d87711f0ed2692de296ca
Rahul Arya <aryarahul819@gmail.com>
2026-07-22 23:38:41 +0530

    fixes

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,48 +1,47 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  lazy = false,
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter").setup({
-      install_dir = vim.fn.stdpath("data") .. "/site",
-    })
+	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter").setup({
+			install_dir = vim.fn.stdpath("data") .. "/site",
+		})
 
-    -- Install parsers for all languages used in this config.
-    -- This is a no-op if parsers are already up to date.
-    require("nvim-treesitter").install({
-      "bash",
-      "c",
-      "cpp",
-      "css",
-      "diff",
-      "go",
-      "gomod",
-      "html",
-      "java",
-      "javascript",
-      "json",
-      "lua",
-      "luadoc",
-      "markdown",
-      "markdown_inline",
-      "python",
-      "query",
-      "regex",
-      "rust",
-      "toml",
-      "tsx",
-      "typescript",
-      "vim",
-      "vimdoc",
-      "yaml",
-    })
+		-- Install parsers for all languages used in this config.
+		-- This is a no-op if parsers are already up to date.
+		require("nvim-treesitter").install({
+			"bash",
+			"c",
+			"cpp",
+			"css",
+			"diff",
+			"go",
+			"gomod",
+			"html",
+			"javascript",
+			"json",
+			"lua",
+			"luadoc",
+			"markdown",
+			"markdown_inline",
+			"python",
+			"query",
+			"regex",
+			"rust",
+			"toml",
+			"tsx",
+			"typescript",
+			"vim",
+			"vimdoc",
+			"yaml",
+		})
 
-    -- Enable treesitter highlighting for all filetypes that have a parser.
-    vim.api.nvim_create_autocmd("FileType", {
-      group = vim.api.nvim_create_augroup("treesitter_attach", { clear = true }),
-      callback = function(ev)
-        pcall(vim.treesitter.start, ev.buf)
-      end,
-    })
-  end,
+		-- Enable treesitter highlighting for all filetypes that have a parser.
+		vim.api.nvim_create_autocmd("FileType", {
+			group = vim.api.nvim_create_augroup("treesitter_attach", { clear = true }),
+			callback = function(ev)
+				pcall(vim.treesitter.start, ev.buf)
+			end,
+		})
+	end,
 }
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,45 +1,48 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  config = function(_, opts)
-    local wk = require("which-key")
-    wk.setup(opts)
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	config = function(_, opts)
+		local wk = require("which-key")
+		wk.setup(opts)
 
-    wk.add({
-      { "<leader>b", group = "buffer" },
-      { "<leader>c", group = "code" },
-      { "<leader>d", group = "debug" },
-      { "<leader>f", group = "find" },
-      { "<leader>g", group = "git" },
-      { "<leader>l", group = "lsp" },
-      { "<leader>s", group = "search" },
-      { "<leader>u", group = "ui/toggles" },
-      { "<leader>w", group = "workspace/session" },
-      { "<leader>x", group = "diagnostics/trouble" },
-      mode = { "n", "v" },
-    })
-  end,
-  opts = {
-    plugins = {
-      marks = true,
-      registers = true,
-      spelling = {
-        enabled = true,
-        suggestions = 20,
-      },
-      presets = {
-        operators = true,
-        motions = true,
-        text_objects = true,
-        windows = true,
-        nav = true,
-        z = true,
-        g = true,
-      },
-    },
-    win = {
-      border = "rounded",
-      padding = { 2, 2 },
-    },
-  },
+		wk.add({
+			{ "<leader>a", group = "ai/sidekick" },
+			{ "<leader>b", group = "buffer" },
+			{ "<leader>c", group = "code" },
+			{ "<leader>d", group = "debug" },
+			{ "<leader>f", group = "find/format" },
+			{ "<leader>g", group = "git" },
+			{ "<leader>l", group = "lsp" },
+			{ "<leader>r", group = "run" },
+			{ "<leader>s", group = "search" },
+			{ "<leader>t", group = "terminal" },
+			{ "<leader>u", group = "ui/toggles" },
+			{ "<leader>w", group = "session" },
+			{ "<leader>x", group = "diagnostics/trouble" },
+			mode = { "n", "v" },
+		})
+	end,
+	opts = {
+		plugins = {
+			marks = true,
+			registers = true,
+			spelling = {
+				enabled = true,
+				suggestions = 20,
+			},
+			presets = {
+				operators = true,
+				motions = true,
+				text_objects = true,
+				windows = true,
+				nav = true,
+				z = true,
+				g = true,
+			},
+		},
+		win = {
+			border = "rounded",
+			padding = { 2, 2 },
+		},
+	},
 }
