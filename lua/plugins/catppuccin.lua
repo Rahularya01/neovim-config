1446b8ac8009a9cb43cb4d570d3f90de77fa755f
Rahularya01 <aryarahul819@gmail.com>
2026-09-07 13:52:51 +0530

    fixes

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -6,7 +6,7 @@ return {
   config = function()
     require("catppuccin").setup({
       flavour = "mocha",
-      transparent_background = false,
+      transparent_background = true,
       integrations = {
         barbar = true,
         blink_cmp = {
